cbcc5ff929b75f85c798261c68e50de422dcfad3
Alexis Guijarro <totonzx@gmail.com>
2022-11-11 12:55:00 -0600

    Continuacion del script de carga

--- scripts/carga_util_act.py
+++ scripts/carga_util_act.py
@@ -11,6 +11,7 @@ from gazebo_msgs.srv import SetModelState,GetModelState
 # Creamos el mensaje para guardar el estado del modelo
 drone_state = ModelState()
 
+
 # Establecer el callback para la deteccion del drone
 def get_drone_state_cb(msg):
     global drone_state
@@ -25,12 +26,14 @@ def main():
     # Creamos la pose para el paquete
     estado_modelo = ModelState()
     estado_modelo = drone_state
-    estado_modelo.pose.position.x = drone_state.pose.position.x = 0.0
-    estado_modelo.pose.position.y = drone_state.pose.position.y = 0.0
+    estado_modelo.pose.position.x = drone_state.pose.position.x
+    estado_modelo.pose.position.y = drone_state.pose.position.y
     estado_modelo.pose.position.z = drone_state.pose.position.z = 0.05
 
     # Esperamos por el servicio de gazebo para obtener el estado de los modelos
     rospy.wait_for_service("/gazebo/get_model_state")
+    get_m_client = rospy.ServiceProxy("/gazebo/get_model_state", ModelState)
 
     # Esperamos por el servicio de gazebo para establecer el estado de los modelos
     rospy.wait_for_service("/gazebo/set_model_state")
+    set_m_client = rospy.ServiceProxy("/gazebo/set_model_state")
